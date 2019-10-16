863c4314bfa03bae5c4855b88e2637e84b6ff48a
Sudharsanan S R <32484479+sudharsanansr@users.noreply.github.com>
2019-10-16 18:21:25 +0530

    Update birthdayCandles.js

--- birthdayCandles.js
+++ birthdayCandles.js
@@ -1,3 +1,24 @@
-Refer to this link
+// Refer to this link
 
-https://www.hackerrank.com/rest/contests/master/challenges/birthday-cake-candles/hackers/argyle_socks/download_solution?primary=true
+// https://www.hackerrank.com/rest/contests/master/challenges/birthday-cake-candles/hackers/argyle_socks/download_solution?primary=true
+
+function processData(input) {
+  let inputLines = input.trim().split('\n');
+  let nCandles = parseInt(inputLines[0]);
+  let candles = inputLines[1].trim().split(' ').map(Number);
+  let candleCount = 0;
+  let max = 0;
+
+  // sort descending order
+  candles.sort((a, b) => b - a);
+
+  // see how many candles have the max height
+  max = candles[0];
+  for (let i = 0; i < nCandles; i++) {
+    if (candles[i] === max) {
+      candleCount++;
+    }
+  }
+
+  console.log(candleCount);
+}
