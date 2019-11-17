95e1d3e2274eeb400193f2f0aa43ab7fba02ccda
Sudharsanan S R <32484479+sudharsanansr@users.noreply.github.com>
2019-11-17 16:35:03 +0530

    Create strangeCounter.js

---
+++ strangeCounter.js
@@ -0,0 +1,24 @@
+let valArr = [];
+valArr.push(null);
+function strangeCounter(n){
+
+    function getValue(n){
+        let arr = [];
+        for(let i = n; i >= 1; i--){
+            arr.push(i);
+        }
+        return arr;
+    }
+
+    getValue(n).forEach(function(element){
+        if(valArr.length > 200){
+            return;
+        }
+        valArr.push(element);
+        if(element === 1){
+            strangeCounter(n * 2);
+        }
+    })
+}
+strangeCounter(3);
+console.log(valArr);
