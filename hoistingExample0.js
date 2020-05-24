cc7387efde1e1818f8388a560476f5c302af09e2
Sudharsanan S R <32484479+sudharsanansr@users.noreply.github.com>
2020-05-24 10:31:38 +0530

    Create hoistingExample0.js

---
+++ hoistingExample0.js
@@ -0,0 +1,19 @@
+var x = 10;
+console.log(typeof x)
+console.log(typeof y)
+console.log(x + y);
+var y = 12;
+
+>>
+
+//Converted as
+
+/*
+var x;
+var y;
+x = 10;
+console.log(typeof x)
+console.log(typeof y)
+console.log(x+y);
+y = 12;
+*/
