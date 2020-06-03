024d561931d9382c026130e8547f3464a1e514da
Sudharsanan Ravichandran <sudharsanv36@gmail.com>
2020-06-03 22:22:24 +0530

    commit-exercise-files

---
+++ es6GettersSetters.js
@@ -0,0 +1,68 @@
+/*
+
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/use-getters-and-setters-to-control-access-to-an-object
+
+ES6: Use getters and setters to Control Access to an Object
+You can obtain values from an object and set the value of a property within an object.
+
+These are classically called getters and setters.
+
+Getter functions are meant to simply return (get) the value of an object's private variable to the user without the user directly accessing the private variable.
+
+Setter functions are meant to modify (set) the value of an object's private variable based on the value passed into the setter function. This change could involve calculations, or even overwriting the previous value completely.
+
+
+class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
+  }
+}
+const lol = new Book('anonymous');
+console.log(lol.writer);  // anonymous
+lol.writer = 'wut';
+console.log(lol.writer);  // wut
+Notice the syntax used to invoke the getter and setter. They do not even look like functions. Getters and setters are important because they hide internal implementation details. Note: It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private.
+
+Use the class keyword to create a Thermostat class. The constructor accepts a Fahrenheit temperature.
+
+Now create a getter and a setter in the class, to obtain the temperature in Celsius.
+
+Remember that C = 5/9 * (F - 32) and F = C * 9.0 / 5 + 32, where F is the value of temperature in Fahrenheit, and C is the value of the same temperature in Celsius.
+
+Note: When you implement this, you will track the temperature inside the class in one scale, either Fahrenheit or Celsius.
+
+This is the power of a getter and a setter. You are creating an API for another user, who can get the correct result regardless of which one you track.
+
+In other words, you are abstracting implementation details from the user.
+
+*/
+
+// Only change code below this line
+class Thermostat{
+  constructor(temperature){
+    this._temperature = temperature;
+  }
+  get temperature(){ //Farenheit to Celsius
+    return ((5/9) * (this._temperature - 32)); 
+  }
+
+  set temperature(updatedTemperature){
+    this._temperature = updatedTemperature * 9/5 + 32; //Celsius to Fahrenheit
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp);
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp);
