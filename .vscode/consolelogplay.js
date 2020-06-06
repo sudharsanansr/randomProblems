4b744c0f87e8695115651d49b8857368a9d69443
Sudharsanan Ravichandran <sudharsanv36@gmail.com>
2020-06-07 02:19:16 +0530

    hands-on testing

---
+++ .vscode/consolelogplay.js
@@ -0,0 +1,18 @@
+var users = [
+    {
+        "name":"John",
+        "age":37
+    },
+    {
+        "name":"Jane",
+        "age":42,
+    },
+    {
+        "name":"Bob",
+        "age":43
+    }
+];
+
+console.table(users);
+console.warn("Hello There, this is a warning!");
+console.error('Hello there, this is a error!');
