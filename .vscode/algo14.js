15da4b999ece57ae746dae409ad570c6f4284d65
Sudharsanan S R <sudharsanv36@gmail.com>
2020-06-15 23:34:44 +0530

    exercise-files

---
+++ .vscode/algo14.js
@@ -0,0 +1,40 @@
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/chunky-monkey
+
+Basic Algorithm Scripting: Chunky Monkey
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+*/
+
+function chunkArrayInGroups(arr, size) {
+    let arrSize = arr.length;
+    let iter = Math.ceil(arrSize/size);
+    let newArr = [];
+    let i = 0;  
+    while(i < iter){
+        let tempArr = [];
+        for(let j = 0; j < size; j++){
+            if(arr.length > 0){ //If arr is not empty
+                tempArr.push(arr.shift());
+            }
+        }
+        i++;
+        newArr.push(tempArr);
+    }
+    console.log(newArr);
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
