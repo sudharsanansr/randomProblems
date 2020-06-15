8fa225870e53769b646ba603e4ced55367394de2
Sudharsanan S R <sudharsanv36@gmail.com>
2020-06-15 22:12:51 +0530

    exercise-files

--- .vscode/algo12.js
+++ .vscode/algo12.js
@@ -27,7 +27,51 @@ getIndexToIns([], 1) should return a number.
 */
 
 function getIndexToIns(arr, num) {
-  return num;
+  console.log(arr);
+  arr.push(num);
+  arr.sort(function(a,b){
+    if(a < b){
+      return -1;
+    }
+    return 1;
+  });
+  console.log(arr);
+  return arr.indexOf(num);
 }
 
-getIndexToIns([40, 60], 50);
+//console.log(getIndexToIns([40, 60], 50));
+//console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
+//console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
+//console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
+//console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
+//console.log(getIndexToIns([40, 60], 50));
+//console.log(getIndexToIns([40, 60], 50));
+//console.log(getIndexToIns([3, 10, 5], 3));
+//console.log(getIndexToIns([3, 10, 5], 3));
+//console.log(getIndexToIns([5, 3, 20, 3], 5));
+//console.log(getIndexToIns([5, 3, 20, 3], 5));
+//console.log(getIndexToIns([2, 20, 10], 19));
+//console.log(getIndexToIns([2, 20, 10], 19));
+//console.log(getIndexToIns([2, 5, 10], 15));
+//console.log(getIndexToIns([2, 5, 10], 15));
+//console.log(getIndexToIns([], 1));
+//console.log(getIndexToIns([], 1));
+
+/*
+[ 10, 20, 30, 40, 50 ]
+[ 10, 20, 30, 40, 50 ]
+[ 10, 20, 30, 40, 50 ]
+[ 10, 20, 30, 40, 50 ]
+[ 40, 60 ]
+[ 40, 60 ]
+[ 3, 10, 5 ]
+[ 3, 10, 5 ]
+[ 5, 3, 20, 3 ]
+[ 5, 3, 20, 3 ]
+[ 2, 20, 10 ]
+[ 2, 20, 10 ]
+[ 2, 5, 10 ]
+[ 2, 5, 10 ]
+[]
+[]
+*/
