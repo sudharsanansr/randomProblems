24e68af1eac157fc5ee4494977e8cf5ac86d9289
Sudharsanan S R <sudharsanv36@gmail.com>
2020-06-25 23:58:50 +0530

    commit files

---
+++ .vscode/ialgo02.js
@@ -0,0 +1,33 @@
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy
+
+Intermediate Algorithm Scripting: Seek and Destroy
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+Note
+You have to use the arguments object.
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
+destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
+destroyer([2, 3, 2, 3], 2, 3) should return [].
+destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"].
+destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65].
+*/
+
+function destroyer(arr) {
+    let newArray = [];
+    for(let i = 1; i < arguments.length; i++){
+        let tempArray = arr.forEach((item,index,array) => {
+            if(array[index] == arguments[i]){
+                newArray.push(index);
+            }
+        });
+    }
+    console.log(newArray.sort());
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);
+
+//Conti
---
+++ .vscode/readme.md
@@ -0,0 +1,10 @@
+Be humble
+Be helpful
+Be thoughtful
+Be creative
+Have your own perspectives
+It's OK to fail
+Fail fast so that you can learn faster
+Be simple
+Be kind
+Whatever happens go forward without regrets
