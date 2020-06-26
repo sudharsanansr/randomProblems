a546f20f10379565679b598d43993746bb53144b
Sudharsanan S R <sudharsanv36@gmail.com>
2020-06-26 20:15:52 +0530

    exercise-files

--- .vscode/googleAlgo00.js
+++ .vscode/googleAlgo00.js
@@ -39,3 +39,5 @@ console.log(sumToCheck([1,2,3,9],8));
 console.log(sumToCheck([1,2,4,4],8));
 console.log(sumToCheck([1,2,4,4],10));
 console.log(sumToCheck([1,8,2,4,4,37],45));
+
+//This will only work if you are looking for a pair of numbers
