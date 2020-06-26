682d6221e7a0313eea791c576d6d41e46d4c16b9
Sudharsanan S R <sudharsanv36@gmail.com>
2020-06-26 19:45:02 +0530

    exercise-files

---
+++ .vscode/googleAlgo00.js
@@ -0,0 +1,41 @@
+/*
+sumToCheck = 8
+Input 1: arr = [1,2,3,9]; should return 'not found'
+Input 2: arr = [1,2,4,4]; should return 'found'
+*/
+
+let sumToCheck = (arr,value) => {
+    arr = arr.sort((a, b) => {
+        if(a < b){
+            return -1;
+        }
+        return 1;
+    })
+
+    let i = 0;
+    let j = arr.length -1;
+    let initialSum = arr[i] + arr[j];
+
+    while((initialSum != value) && (i != j)){
+        if(initialSum > value){
+            j--;
+            initialSum = arr[i] + arr[j];
+        }
+        else{
+            i++;
+            initialSum = arr[i]+arr[j];
+        }
+    }
+
+    if(initialSum == value){
+        return [i,j];
+    }
+    else{
+        return 'not found';
+    }
+}
+
+console.log(sumToCheck([1,2,3,9],8));
+console.log(sumToCheck([1,2,4,4],8));
+console.log(sumToCheck([1,2,4,4],10));
+console.log(sumToCheck([1,8,2,4,4,37],45));
