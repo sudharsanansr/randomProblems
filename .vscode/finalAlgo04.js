2b22846522b667ace126415a9172d4b7d9e1484b
Sudharsanan S R <sudharsanv36@gmail.com>
2020-07-05 19:43:27 +0530

    exercise-files

--- .vscode/finalAlgo04.js
+++ .vscode/finalAlgo04.js
@@ -45,92 +45,89 @@ checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUAR
 checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
 */
 
+// Create an array of objects which hold the denominations and their values
+var denom = [
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 }
+];
+
 function checkCashRegister(price, cash, cid) {
-  let remainder = cash - price;
-  console.log(remainder);
-  let valueSet = [
-      {
-          type : 'PENNY',
-          value : 0.01
-      },
-      {
-          type : 'NICKEL',
-          value : 0.05
-      },
-      {
-          type : 'DIME',
-          value : 0.1
-      },
-      {
-          type : 'QUARTER',
-          value : 0.25
-      },
-      {
-          type : 'ONE',
-          value : 1
-      },
-      {
-          type : 'FIVE',
-          value : 5
-      },
-      {
-          type : 'TEN',
-          value : 10
-      },
-      {
-          type : 'TWENTY',
-          value : 20
-      },
-      {
-          type : 'ONE HUNDRED',
-          value : 100
-      }
-  ]
+  var output = { status: null, change: [] };
+  var change = cash - price;
 
-  valueSet.sort((a,b) => {
-      if(a.value > b.value){
-          return -1;
-      }
-      return 1;
-  });
+  // Transform CID array into drawer object
+  var register = cid.reduce(
+    function(acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
 
-  let cidObj = cid.reduce((initialValue,item) => {
-      if(typeof initialValue[item[0]] == 'undefined'){
-          initialValue[item[0]] = item[1];
-      }
-      return initialValue;
-  },{});
-
-  let relevantItem = valueSet.filter(item => item.value < remainder);
-
-  console.log(cidObj);
-  console.log(relevantItem);
-
-  let tempRemainder = remainder;
-
-  let returnObj = {};
-  returnObj.status = '';
-  returnObj.change = [];
-
-  for(let i = 0; i < relevantItem.length; i++){
-      let tempArray = [];
-      if(tempRemainder > 0){
-        if(tempRemainder % relevantItem[i].value == 0){
-            tempArray[0] = relevantItem[i].type;
-            tempArray[1] = (relevantItem[i].value * (tempRemainder / relevantItem[i].value));
-        }
-        else{
-            tempRemainder = tempRemainder - (tempRemainder / relevantItem[i].value);
-            tempArray[0] = relevantItem[i].type;
-            tempArray[1] = (relevantItem[i].value * (tempRemainder / relevantItem[i].value));
-        }
-        console.log(tempRemainder);
-        returnObj.change.push(tempArray);
-      }
+  // Handle exact change
+  if (register.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+    return output;
   }
 
-  console.log(returnObj);
+  // Handle obvious insufficient funds
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Loop through the denomination array
+  var change_arr = denom.reduce(function(acc, curr) {
+    var value = 0;
+    // While there is still money of this type in the drawer
+    // And while the denomination is larger than the change remaining
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+
+      // Round change to the nearest hundreth deals with precision errors
+      change = Math.round(change * 100) / 100;
+    }
+    // Add this denomination to the output only if any was used.
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc; // Return the current change_arr
+  }, []); // Initial value of empty array for reduce
+
+  // If there are no elements in change_arr or we have leftover change, return
+  // the string "Insufficient Funds"
+  if (change_arr.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Here is your change, ma'am.
+  output.status = "OPEN";
+  output.change = change_arr;
+  console.log(output);
+  return output;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01],["NICKEL", 2.05],["DIME", 3.1],["QUARTER", 4.25],["ONE", 90],["FIVE", 55],["TEN", 20],["TWENTY", 60],["ONE HUNDRED", 100]]);
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+// test here
+checkCashRegister(19.5, 20.0, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90.0],
+  ["FIVE", 55.0],
+  ["TEN", 20.0],
+  ["TWENTY", 60.0],
+  ["ONE HUNDRED", 100.0]
+]);
