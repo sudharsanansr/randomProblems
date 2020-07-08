ff7893f02fce83e30f12e90a489791286c85bd2c
Sudharsanan S R <sudharsanv36@gmail.com>
2020-07-08 10:12:26 +0530

    exercise-file

---
+++ twitBits03.js
@@ -0,0 +1,7 @@
+var myNum = 10;
+
+function myName(){
+    console.log(10/5);
+}
+
+console.log(myNum);
---
+++ twitBits04.js
@@ -0,0 +1,21 @@
+//printStars(4);
+//printStars(6);
+printStars(0);
+/*
+
+* 
+* *
+* * *
+* * * *
+
+*/
+
+function printStars(num){
+    for(let i = 0; i < num; i++){
+        let arr = [];
+        for(j = -1; j < i; j++){
+            arr.push('*');
+        }
+        console.log(arr.join(" "));
+    }
+}
