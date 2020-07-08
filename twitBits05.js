7975d2dd78c0bf0c5564dc7eaede87bccb40ec37
Sudharsanan S R <sudharsanv36@gmail.com>
2020-07-08 18:03:55 +0530

    less verbose update

--- twitBits05.js
+++ twitBits05.js
@@ -11,10 +11,8 @@ function printStars(num){
     let i = 0;
     let j = num;
     while(i < num){
-      let str = '';
-      str += ' '.repeat(j-1);
-      str += '*'.repeat(i+1);
-      console.log(str.split('').join(' '));
+      let str = ' '.repeat(j-1).concat('*'.repeat(i+1)).split('').join(' ');
+      console.log(str);
       i++;
       j--;
     }
