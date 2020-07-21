1ee316f037020fe234e09d2ba75a6b4464a3bdc1
Sudharsanan S R <sudharsanv36@gmail.com>
2020-07-21 23:17:09 +0530

    exercise-files

---
+++ twitBits18.js
@@ -0,0 +1,8 @@
+console.log(1 / 20000000);
+console.log(parseInt(1 / 2000000));
+console.log(1999);
+console.log(1 / 1999999);
+console.log(parseInt(1 / 1999999));
+console.log({} + []);
+console.log([] + {});
+console.log(0.1 + 0.2);
