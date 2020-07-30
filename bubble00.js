c59771be45a54345e310da4b74afdc1ddea12a68
Sudharsanan S R <sudharsanv36@gmail.com>
2020-07-30 18:52:15 +0530

    exercise-file

--- bubble00.js
+++ bubble00.js
@@ -1,15 +1,22 @@
 //My own bubble sort, the concept is to test whether the index and index + 1 is greater? If greater, swap the values!
 
-let arr = [8,4,5,1,2,37,64,22,6,89,102];
-let size = arr.length;
-while(size > 0){
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > arr[i+1]){
-            let temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
+let arrays = [[8,4,5,1,2,37,64,22,6,89,102],[8,4,5,1,2,37,64,22,6,89,102,1]];
+
+arrays.forEach((el) => {
+    console.log(bubbleSort(el));
+});
+
+function bubbleSort(arr){
+    let size = arr.length;
+    while(size > 0){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
         }
+        size--;
     }
-    size--;
-    console.log(arr);
+    return arr;
 }
