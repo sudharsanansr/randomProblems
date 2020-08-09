d95c8db0c8ff05770f3f7bbd9cf92f7c4d2d51c1
Sudharsanan S R <sudharsanv36@gmail.com>
2020-08-09 22:06:58 +0530

    exercise-file

---
+++ py07.py
@@ -0,0 +1,16 @@
+import os
+import time
+ 
+path = r"E:\mid_surfsedev\agent\export\syslog" #folder path
+now = time.time() #current time
+
+for entry in os.scandir(path):
+    filepath = path+'\\'+entry.name; #absolute file path
+    fileupdated = os.path.getmtime(filepath) #file last updated
+    timediff = now - fileupdated #time diff in epoch
+
+    if entry.is_file(): #Extra validation to check if that's a file
+        if(timediff > 172800): #Any files created minimum 2 days before
+            print('file path: '+filepath)
+            #print('file removed: '+filepath)
+            #os.remove(filepath)
