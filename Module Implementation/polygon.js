13b2f78f1c7e22b4324d3e980d80606af86741ff
Sudharsanan S R <sudharsanv36@gmail.com>
2020-08-13 00:59:58 +0530

    deletes

--- Module Implementation/polygon.js
+++
@@ -1,7 +0,0 @@
-export default class Polygon{
-    constructor(name){
-        this.name = name;
-    }
-
-    getName = () => this.name || 'undefined';
-}
--- Module Implementation/square.js
+++
@@ -1,9 +0,0 @@
-class Square extends Polygon{
-    constructor(name,side){
-        super(name);
-        this.side = side;
-    }
-    getArea = () => Math.pow(this.side,2);
-
-    getPerimeter = () => 4 * this.side;
-}
